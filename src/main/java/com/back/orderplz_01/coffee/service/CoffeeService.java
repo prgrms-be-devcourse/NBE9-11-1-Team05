784a68dc6d2bda5cf61448828875553ba0755eea
sorbet0404 <cyclebae@naver.com>
2026-03-21 18:34:01 +0900

    feat: CUS-02 원두 상세정보 출력 기능 구현 및 테스트 완료

--- src/main/java/com/back/orderplz_01/coffee/service/CoffeeService.java
+++ src/main/java/com/back/orderplz_01/coffee/service/CoffeeService.java
@@ -1,5 +1,8 @@
 package com.back.orderplz_01.coffee.service;
 
+import com.back.orderplz_01.coffee.dto.CoffeeDetailDto;
+import com.back.orderplz_01.coffee.entity.Coffee;
+import com.back.orderplz_01.coffee.repository.CoffeeRepository;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -7,4 +10,17 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class CoffeeService {
+    private final CoffeeRepository coffeeRepository;
+
+    public CoffeeDetailDto getCoffeeDetail(Long id) {
+        Coffee coffee = coffeeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException( id+"번 원두는 존재하지 않습니다."));
+
+        return new CoffeeDetailDto(
+                coffee.getName(),
+                coffee.getDescription(),
+                coffee.getPrice(),
+                coffee.getQuantity()
+        );
+    }
 }
