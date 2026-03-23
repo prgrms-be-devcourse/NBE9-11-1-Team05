959a8783145299b5da1f468a59c589e38203828c
zioJioh <117734133+zioJioh@users.noreply.github.com>
2026-03-23 11:46:06 +0900

    Feat/ own 10 (#15)
    * feat: 주문 상세 페이지 조회
    
    * refactor: 메서드 이름, 반환 형태 변경

--- src/main/java/com/back/orderplz_01/orders/controller/OrdersController.java
+++ src/main/java/com/back/orderplz_01/orders/controller/OrdersController.java
@@ -1,7 +1,12 @@
 package com.back.orderplz_01.orders.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.back.orderplz_01.orders.dto.res.OrdersDetailRes;
+import com.back.orderplz_01.orders.service.OrdersService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -11,4 +16,11 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/orderss")
 @Tag(name = "OrdersController", description = "주문 API")
 public class OrdersController {
+
+	private final OrdersService ordersService;
+
+	@GetMapping("/{ordersId}")
+	public OrdersDetailRes ordersDetail(@PathVariable Long ordersId) {
+		return ordersService.ordersDetail(ordersId);
+	}
 }
--- src/main/java/com/back/orderplz_01/orders/service/OrdersService.java
+++ src/main/java/com/back/orderplz_01/orders/service/OrdersService.java
@@ -2,9 +2,23 @@ package com.back.orderplz_01.orders.service;
 
 import org.springframework.stereotype.Service;
 
+import com.back.orderplz_01.orders.dto.res.OrdersDetailRes;
+import com.back.orderplz_01.orders.entity.Orders;
+import com.back.orderplz_01.orders.repository.OrdersRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class OrdersService {
+
+	private final OrdersRepository ordersRepository;
+
+	public OrdersDetailRes ordersDetail(Long ordersId) {
+		Orders orders = ordersRepository.findById(ordersId)
+			.orElseThrow(() -> new EntityNotFoundException("존재하지 않는 주문입니다."));
+
+		return OrdersDetailRes.from(orders);
+	}
 }
