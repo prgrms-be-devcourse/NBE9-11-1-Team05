f7fdeb2885f8e72b34257fe387c42df575c32d11
sorbet0404 <151494073+sorbet0404@users.noreply.github.com>
2026-03-23 11:20:38 +0900

    Revert "feat: CUS-02 원두 상세정보 출력 기능 구현 및 테스트 완료 (#11)"
    This reverts commit 414842bfa8da642ce8666526be74ac9aeaf7e587.

--- src/main/java/com/back/orderplz_01/coffee/service/CoffeeService.java
+++ src/main/java/com/back/orderplz_01/coffee/service/CoffeeService.java
@@ -1,8 +1,5 @@
 package com.back.orderplz_01.coffee.service;
 
-import com.back.orderplz_01.coffee.dto.CoffeeDetailDto;
-import com.back.orderplz_01.coffee.entity.Coffee;
-import com.back.orderplz_01.coffee.repository.CoffeeRepository;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -10,17 +7,4 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class CoffeeService {
-    private final CoffeeRepository coffeeRepository;
-
-    public CoffeeDetailDto getCoffeeDetail(Long id) {
-        Coffee coffee = coffeeRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException( id+"번 원두는 존재하지 않습니다."));
-
-        return new CoffeeDetailDto(
-                coffee.getName(),
-                coffee.getDescription(),
-                coffee.getPrice(),
-                coffee.getQuantity()
-        );
-    }
 }
--- src/main/java/com/back/orderplz_01/global/springDoc/SpringDoc.java
+++ src/main/java/com/back/orderplz_01/global/springDoc/SpringDoc.java
@@ -1,22 +1,12 @@
 package com.back.orderplz_01.global.springDoc;
 
-import io.swagger.v3.oas.annotations.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "주문하시조 API", version = "beta", description = "1차 프로젝트 API"))
 public class SpringDoc {
 
-    // 1. 고객용 API 그룹 설정
-    @Bean
-    public GroupedOpenApi customerApi() {
-        return GroupedOpenApi.builder()
-                .group("고객용 API") // 우측 상단 선택창에 보여질 이름
-                .pathsToMatch("/api/coffees/**")       // 이 그룹에 묶어줄 API 주소 패턴 (컨트롤러 주소에 맞춤)
-                .build();
-    }
 }
