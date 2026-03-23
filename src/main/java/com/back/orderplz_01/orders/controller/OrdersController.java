5022a426b1f91b9b01e9a07647754539f7114e9e
gioJioh <sjgio0515@naver.com>
2026-03-23 11:05:11 +0900

    refactor: 메서드 이름, 반환 형태 변경

--- src/main/java/com/back/orderplz_01/orders/controller/OrdersController.java
+++ src/main/java/com/back/orderplz_01/orders/controller/OrdersController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.back.orderplz_01.orders.dto.res.OrdersRes;
+import com.back.orderplz_01.orders.dto.res.OrdersDetailRes;
 import com.back.orderplz_01.orders.service.OrdersService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,7 +20,7 @@ public class OrdersController {
 	private final OrdersService ordersService;
 
 	@GetMapping("/{ordersId}")
-	public OrdersRes detail(@PathVariable Long ordersId) {
+	public OrdersDetailRes ordersDetail(@PathVariable Long ordersId) {
 		return ordersService.ordersDetail(ordersId);
 	}
 }
--- src/main/java/com/back/orderplz_01/orders/service/OrdersService.java
+++ src/main/java/com/back/orderplz_01/orders/service/OrdersService.java
@@ -2,10 +2,11 @@ package com.back.orderplz_01.orders.service;
 
 import org.springframework.stereotype.Service;
 
-import com.back.orderplz_01.orders.dto.res.OrdersRes;
+import com.back.orderplz_01.orders.dto.res.OrdersDetailRes;
 import com.back.orderplz_01.orders.entity.Orders;
 import com.back.orderplz_01.orders.repository.OrdersRepository;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -14,16 +15,10 @@ public class OrdersService {
 
 	private final OrdersRepository ordersRepository;
 
-	public OrdersRes ordersDetail(Long ordersId) {
+	public OrdersDetailRes ordersDetail(Long ordersId) {
 		Orders orders = ordersRepository.findById(ordersId)
-			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 주문입니다."));
+			.orElseThrow(() -> new EntityNotFoundException("존재하지 않는 주문입니다."));
 
-		return new OrdersRes(
-			orders.getId(),
-			orders.getEmail(),
-			orders.getAddress(),
-			orders.getZipCode(),
-			orders.getOrderedAt()
-		);
+		return OrdersDetailRes.from(orders);
 	}
 }
