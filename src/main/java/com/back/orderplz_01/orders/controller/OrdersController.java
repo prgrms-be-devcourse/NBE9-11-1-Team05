5da487b464c5c6b750382ac29889bc6120b517e2
zioJioh <117734133+zioJioh@users.noreply.github.com>
2026-03-23 15:45:33 +0900

    refactor: 주문, 주문 품목 엔티티 변경 (#22)

--- src/main/java/com/back/orderplz_01/orders/controller/OrdersController.java
+++ src/main/java/com/back/orderplz_01/orders/controller/OrdersController.java
@@ -2,22 +2,13 @@ package com.back.orderplz_01.orders.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.back.orderplz_01.global.apiRes.ApiRes;
-import com.back.orderplz_01.orders.dto.request.CoffeeOrderReq;
+import com.back.orderplz_01.orders.dto.res.OrdersDetailRes;
 import com.back.orderplz_01.orders.service.OrdersService;
 
-import io.swagger.v3.oas.annotations.Operation;
-import com.back.orderplz_01.orders.dto.res.OrdersDetailRes;
-
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -28,12 +19,12 @@ public class OrdersController {
 
 	private final OrdersService ordersService;
 
-	@PostMapping
-	@Operation(summary = "원두 결제하기")
-	@Transactional
-	public ResponseEntity<ApiRes<Void>> pay(@RequestBody @Valid CoffeeOrderReq req) {
-		return ordersService.pay(req);
-	}
+	// @PostMapping
+	// @Operation(summary = "원두 결제하기")
+	// @Transactional
+	// public ResponseEntity<ApiRes<Void>> pay(@RequestBody @Valid CoffeeOrderReq req) {
+	// 	return ordersService.pay(req);
+	// }
 
 	@GetMapping("/{ordersId}")
 	public OrdersDetailRes ordersDetail(@PathVariable Long ordersId) {
--- src/main/java/com/back/orderplz_01/orders/entity/Orders.java
+++ src/main/java/com/back/orderplz_01/orders/entity/Orders.java
@@ -1,18 +1,19 @@
 package com.back.orderplz_01.orders.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 
-import com.back.orderplz_01.coffee.entity.Coffee;
 import com.back.orderplz_01.global.entity.BaseEntity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -39,19 +40,15 @@ public class Orders extends BaseEntity {
 	@Column(nullable = false)
 	private Long totalAmount;
 
-	@Column(nullable = false)
-	private Long quantity;
-
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	OrderStatus orderStatus;
 
-	@ManyToOne
-	@JoinColumn(name = "coffee_id", nullable = false)
-	private Coffee coffee;
+	@OneToMany(mappedBy = "orders", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+	private List<OrdersItem> orderItems = new ArrayList<>();
 
-	public void addQuantityAndAmount(Long quantity, Long amount) {
-		this.quantity += quantity;
-		this.totalAmount += amount;
-	}
+	// public void addQuantityAndAmount(Long quantity, Long amount) {
+	// 	this.quantity += quantity;
+	// 	this.totalAmount += amount;
+	// }
 }
