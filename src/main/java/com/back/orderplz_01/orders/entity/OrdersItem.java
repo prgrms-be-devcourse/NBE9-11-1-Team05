641bf6a4cbb8165d9326f3249b741e36864319f9
gioJioh <sjgio0515@naver.com>
2026-03-23 15:44:25 +0900

    refactor: 주문, 주문 품목 엔티티 변경

---
+++ src/main/java/com/back/orderplz_01/orders/entity/OrdersItem.java
@@ -0,0 +1,33 @@
+package com.back.orderplz_01.orders.entity;
+
+import com.back.orderplz_01.coffee.entity.Coffee;
+import com.back.orderplz_01.global.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrdersItem extends BaseEntity {
+
+	@Column(nullable = false)
+	private Long quantity;
+
+	@Column(nullable = false)
+	private Long price;
+
+	@ManyToOne
+	@JoinColumn(name = "orders_id")
+	private Orders orders;
+
+	@ManyToOne
+	@JoinColumn(name = "coffee_id")
+	private Coffee coffee;
+}
