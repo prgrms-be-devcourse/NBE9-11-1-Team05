39caa4adf60a902311c3fe45123f6c7f02513e4c
studywithme <tea_meen1@daum.net>
2026-03-24 15:21:12 +0900

    feature: cus-09 고객 주문목록 조회 API (#39)
    CUS-09: 고객 주문목록조회 API
    - OrderSerach

--- src/main/java/com/back/orderplz_01/orders/controller/OrdersController.java
+++ src/main/java/com/back/orderplz_01/orders/controller/OrdersController.java
@@ -1,18 +1,26 @@
 package com.back.orderplz_01.orders.controller;
 
+import com.back.orderplz_01.orders.dto.request.OrderSearchRequestDto;
 import com.back.orderplz_01.global.apiRes.ApiRes;
 import com.back.orderplz_01.orders.dto.request.OrderStatusChangeReq;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import com.back.orderplz_01.orders.dto.res.OrdersDetailRes;
+import com.back.orderplz_01.orders.dto.response.OrdersSearchListRes;
 import com.back.orderplz_01.orders.service.OrdersService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +33,12 @@ public class OrdersController {
 	@GetMapping("/{ordersId}")
 	public OrdersDetailRes ordersDetail(@PathVariable Long ordersId) {
 		return ordersService.ordersDetail(ordersId);
+	}
+
+	/** CUS-09 주문 검색 API */
+	@PostMapping("/search")
+	public OrdersSearchListRes search(@Valid @RequestBody OrderSearchRequestDto request) {
+		return ordersService.search(request);
 	}
 
 	// OWN-04 : 주문 상태 변경 API
